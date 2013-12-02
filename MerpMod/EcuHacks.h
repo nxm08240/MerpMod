86d705415a6859d329f2379bb95fed87eaf7a5e2
Merp <merrillamyersiii@gmail.com>
2013-12-02 00:29:45 -0500

    Created TODO.md and moved major TODO items from EcuHacks.h.

--- MerpMod/EcuHacks.h
+++ MerpMod/EcuHacks.h
@@ -12,24 +12,6 @@
     GNU General Public License for more details.
 */
 
-////////////////////////////
-//TODO LIST:
-////////////////////////////
-
-/*
-
-SHORT TERM:
-1. FBKC disable for LC??
-2. Document programming mode & ram tuning API, and idatohew instructions
-3. Document sharptune API
-4. Write new unit tests for: RamTuning, BoostHacks, FuelHacks, TimingHacks
-5. Write a fuel-cut startup interlocker based on user defined input sequence
-	-Will require some tricky memory management (reset clear, etc)
-6. Figure out make-file building, use ENV vars to set TARGET ROM
-7. Use a batch file to run multi-make-builds, set build config in ENV vars
-	-This will require a new 'update-rom' batch file logic path to direct binary output (use ENV var in batch file!)
-*/
-
 #define ROMCODE  __attribute__ ((section ("RomHole_Code"),aligned(4)))
 #define DEFDATA __attribute__ ((section ("DefinitionData"),aligned(4)))
 #define METADATA  __attribute__ ((section ("MetaData"),aligned(4)))
